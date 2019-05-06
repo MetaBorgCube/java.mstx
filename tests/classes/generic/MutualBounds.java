09eea90df0d2966e647801b3e1d34387a33e0cfe
Arjen Rouvoet <a.j.rouvoet@gmail.com>
2019-05-06 22:17:55 +0200

    Fix mutualbounds test

--- tests/classes/generic/MutualBounds.java
+++ tests/classes/generic/MutualBounds.java
@@ -1,11 +1,11 @@
 /* Mutually recursive generic bounds (pg 212, Example 8.1.2-1) */
 interface ConvertibleTo<T> {
-    T convert();
+    // T convert();
 }
 
 class ReprChange<T extends ConvertibleTo<S>, S extends ConvertibleTo<T>> {
-    T t;
+//     T t;
 
-    void set(S s) { t = s.convert(); }
-    S get() { return t.convert(); }
+//     void set(S s) { t = s.convert(); }
+//     S get() { return t.convert(); }
 }
